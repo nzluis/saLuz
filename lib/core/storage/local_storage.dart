939fb6f2015324993058c26304e65b4694540e05
Luis Navarro <luis.navarro@fashiola.com>
2026-09-17 12:09:02 +0200

    feat: add SharedPreferences local storage wrapper

---
+++ lib/core/storage/local_storage.dart
@@ -0,0 +1,31 @@
+import 'package:shared_preferences/shared_preferences.dart';
+
+class LocalStorage {
+  const LocalStorage(this._prefs);
+
+  final SharedPreferences _prefs;
+
+  static Future<LocalStorage> init() async {
+    final prefs = await SharedPreferences.getInstance();
+    return LocalStorage(prefs);
+  }
+
+  String? getString(String key) => _prefs.getString(key);
+
+  Future<bool> setString(String key, String value) =>
+      _prefs.setString(key, value);
+
+  int? getInt(String key) => _prefs.getInt(key);
+
+  Future<bool> setInt(String key, int value) => _prefs.setInt(key, value);
+
+  bool? getBool(String key) => _prefs.getBool(key);
+
+  Future<bool> setBool(String key, bool value) => _prefs.setBool(key, value);
+
+  Future<bool> remove(String key) => _prefs.remove(key);
+
+  Future<bool> clear() => _prefs.clear();
+
+  bool containsKey(String key) => _prefs.containsKey(key);
+}
